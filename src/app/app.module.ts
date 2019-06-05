b2156d27866a82de299a7597a16e50c04a2efac9
Benjamin Dauvissat <b.dauvissat@gmail.com>
2019-06-05 12:28:40 +0200

    Component for listing Elasticsearch indices

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IndicesComponent } from './indices/indices.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IndicesComponent
   ],
   imports: [
     BrowserModule,
