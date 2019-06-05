e599cbe19b4f72e2ffd0ad6207b52e9d75d55f1d
Benjamin Dauvissat <b.dauvissat@gmail.com>
2019-06-05 16:34:19 +0200

    Trying to connect to ES

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,15 @@
-import { Component } from '@angular/core';
+import {Component, NgModule} from '@angular/core';
+import { IndicesComponent } from './indices/indices.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
+})
+@NgModule({
+  declarations: [
+    IndicesComponent
+  ]
 })
 export class AppComponent {
   title = 'ihmelastic';
--- src/app/indices/indices.component.ts
+++ src/app/indices/indices.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import {Client} from "elasticsearch";
+
+declare var require: any;
 
 @Component({
   selector: 'app-indices',
   templateUrl: './indices.component.html',
   styleUrls: ['./indices.component.css']
 })
+
 export class IndicesComponent implements OnInit {
 
+  private client: Client;
+  message: string = "coucou";
   constructor() { }
 
   ngOnInit() {
+    this.client = new Client({
+      host: 'http://localhost:9200',
+      log: 'trace'
+    });
+
+    this.client.ping({
+      requestTimeout: 30000,
+    }, function (error) {
+      if (error) {
+        this.message = 'elasticsearch cluster is down!';
+      } else {
+        this.message = 'All is well';
+      }
+    });
+
   }
 
 }
